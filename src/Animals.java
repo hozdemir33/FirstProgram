1f3d8b7604cab79cc7cc8f4c2cae5f7beed5f399
hozdemir33 <hozdemir8233@gmail.com>
2024-08-04 23:44:17 -0700

    second commit

--- src/Animals.java
+++ src/Animals.java
@@ -4,8 +4,11 @@ public class Animals {
     public static void main(String[] args) {
 
 
+        Birds b1=new Birds("birdy", "YELLOW", 36);
 
-
+        System.out.println(b1.getAge());
+        System.out.println(b1.getName());
+        System.out.println(b1.getColor());
 
     }
 }
--- src/Birds.java
+++ src/Birds.java
@@ -1,9 +1,38 @@
 public class Birds {
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     private String name;
     private String color;
-    public  String age;
+    public  int age;
 
+    public Birds(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
 
 
 }
